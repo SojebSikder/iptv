5c61b731971e10627bb469ad3e6e6ce62c2e741b
sojebsikder <sojebsikder@gmail.com>
2021-06-21 21:05:18 +0600

    main

--- lib/pages/video_player/video_player.dart
+++ lib/pages/video_player/video_player.dart
@@ -1,6 +1,7 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:iptv/config/app_config.dart';
+import 'package:iptv/widgets/ChannelList.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerPage extends StatefulWidget {
@@ -14,13 +15,7 @@ class VideoPlayerPage extends StatefulWidget {
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
   // late TargetPlatform
   //     _platform;
-  late VideoPlayerController
-      _videoPlayerController1; //Wuhan live broadcast //Control instantiation is the same as below
-  // late VideoPlayerController _videoPlayerController2; //CCTV13
-  // late VideoPlayerController _videoPlayerController3; //CCTV1
-  // late VideoPlayerController _videoPlayerController4; //CCTV6
-  // late VideoPlayerController _videoPlayerController5; //CCTV5
-  // late VideoPlayerController _videoPlayerController6; //Huya Movie
+  late VideoPlayerController _videoPlayerController1;
   late ChewieController _chewieController;
 
   var channels = [
@@ -37,9 +32,7 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   @override
   void initState() {
     super.initState();
-    // _videoPlayerController1 = VideoPlayerController.network(
-    //     'https://tv-trthaber.live.trt.com.tr/master.m3u8');
-
+    // Initial Channel link
     _videoPlayerController1 = VideoPlayerController.network('${widget.link}');
 
     _chewieController = ChewieController(
@@ -85,7 +78,7 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       ),
       body: ListView(
         shrinkWrap: true,
-        children: <Widget>[
+        children: [
           Container(
             height: 200.0,
             child: Center(
@@ -102,54 +95,48 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
             child: Text('Fullscreen'),
           ),
           ListView.builder(
-              shrinkWrap: true,
-              itemCount: channels.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      onPressed: () {
+            shrinkWrap: true,
+            itemCount: channels.length,
+            itemBuilder: (context, index) {
+              return Container(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        //State
+                        _chewieController.dispose();
+
+                        // _videoPlayerController2
+                        //     .pause(); //The second playback function is paused
+                        // _videoPlayerController2.seekTo(
+                        //     Duration(seconds: 0)); //Set the progress bar to 0
+
                         setState(() {
-                          //State
-                          _chewieController.dispose();
-
-                          // _videoPlayerController2
-                          //     .pause(); //The second playback function is paused
-                          // _videoPlayerController2.seekTo(
-                          //     Duration(seconds: 0)); //Set the progress bar to 0
-                          // _videoPlayerController3.pause();
-                          // _videoPlayerController3.seekTo(Duration(seconds: 0));
-                          // _videoPlayerController4.pause();
-                          // _videoPlayerController4.seekTo(Duration(seconds: 0));
-                          // _videoPlayerController5.pause();
-                          // _videoPlayerController5.seekTo(Duration(seconds: 0));
-                          // _videoPlayerController6.pause();
-                          // _videoPlayerController6.seekTo(Duration(seconds: 0));
-
-                          setState(() {
-                            _videoPlayerController1 =
-                                VideoPlayerController.network(
-                                    "${channels[index]['link']}");
-                          });
-                          _chewieController = ChewieController(
-                            videoPlayerController:
-                                _videoPlayerController1, //Control the first playback control
-                            aspectRatio: 3 / 2,
-                            autoPlay: true,
-                            looping: true,
-                          );
+                          _videoPlayerController1 =
+                              VideoPlayerController.network(
+                                  "${channels[index]['link']}");
                         });
-                      },
-                      child: Padding(
-                        child: Text("${channels[index]['title']}"),
-                        padding: EdgeInsets.symmetric(vertical: 16.0),
-                      ),
+                        _chewieController = ChewieController(
+                          videoPlayerController:
+                              _videoPlayerController1, //Control the first playback control
+                          aspectRatio: 3 / 2,
+                          autoPlay: true,
+                          looping: true,
+                        );
+                      });
+                    },
+                    child: Padding(
+                      child: Text("${channels[index]['title']}"),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
                     ),
                   ),
-                );
-              }),
+                ),
+              );
+            },
+          ),
+          // ChannelListWidget(),
         ],
       ),
     );
